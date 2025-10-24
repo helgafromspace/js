63607b3aa4b53439f4feaf676e08a04a7375aabc
olhasemenova <olha.sergeevna87@gmail.com>
2025-10-24 21:41:02 +0300

    22

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let a = 10
-console.log(a)
+// let a = 10;
+// console.log(a);
 
-var message; //Об'ява змінної "age"
-message = 'Hello world'; //Ініціалізація змінної значенням "Hello world"
-console.log(message)
+// var message; //Об'ява змінної "age"
+// message = 'Hello world'; //Ініціалізація змінної значенням "Hello world"
+// console.log(message)
 
 // let b = 5;
 // console.log(b);
