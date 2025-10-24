2977da9a24034e6e9effc42ed61c6ff797fbb0f8
olhasemenova <olha.sergeevna87@gmail.com>
2025-10-24 21:41:44 +0300

    3

--- script.js
+++ script.js
@@ -18,17 +18,17 @@ const num1 = 10;
 const num2 = 5;
 
 //створіть тут змінну що зберігає постійні значення з ім'ям sum яка має зберігати суму чисел
-let sum = num1 + num2
-console.log(sum)
+let sum = num1 + num2;
+console.log(sum);
 //створіть тут змінну що зберігає постійні значення з ім'ям difference яка має зберігати різницю чисел
-let difference = num1 - num2
-console.log(difference)
+let difference = num1 - num2;
+console.log(difference);
 //створіть тут змінну що зберігає постійні значення з ім'ям product яка має зберігати добуток чисел
-let product = num1 * num2
-console.log(product)
+let product = num1 * num2;
+console.log(product);
 //створіть тут змінну що зберігає постійні значення з ім'ям quotient яка має зберігати частку чисел
-let quotient = num1 / num2
-console.log(quotient)
+let quotient = num1 / num2;
+console.log(quotient);
 
-let yearOfBirth = 1900
-console.log (typeof (yearOfBirth))
+let yearOfBirth = 1900;
+console.log (typeof (yearOfBirth));
