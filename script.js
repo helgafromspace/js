19f5ef78dbde40176ee75c472336b4c638f7a4f9
olhasemenova <olha.sergeevna87@gmail.com>
2025-10-24 21:40:37 +0300

    2

--- script.js
+++ script.js
@@ -5,5 +5,30 @@ var message; //Об'ява змінної "age"
 message = 'Hello world'; //Ініціалізація змінної значенням "Hello world"
 console.log(message)
 
-let b = 5
-console.log(b)
+// let b = 5;
+// console.log(b);
+
+// let c = 7;
+// console.log(c);
+
+// let y = null
+// console.log(+null)
+
+const num1 = 10;
+const num2 = 5;
+
+//створіть тут змінну що зберігає постійні значення з ім'ям sum яка має зберігати суму чисел
+let sum = num1 + num2
+console.log(sum)
+//створіть тут змінну що зберігає постійні значення з ім'ям difference яка має зберігати різницю чисел
+let difference = num1 - num2
+console.log(difference)
+//створіть тут змінну що зберігає постійні значення з ім'ям product яка має зберігати добуток чисел
+let product = num1 * num2
+console.log(product)
+//створіть тут змінну що зберігає постійні значення з ім'ям quotient яка має зберігати частку чисел
+let quotient = num1 / num2
+console.log(quotient)
+
+let yearOfBirth = 1900
+console.log (typeof (yearOfBirth))
