5338e9b8747fe8758dc591159a53c532bc8002e5
olhasemenova <olha.sergeevna87@gmail.com>
2025-11-09 13:41:48 +0200

    add select and radiobuttons example

--- .idea/hw11.1/forms/main.js
+++ .idea/hw11.1/forms/main.js
@@ -1,5 +1,5 @@
 'use strict'
-const form = document.querySelector('form');
+// const form = document.querySelector('form');
 // form.addEventListener('submit', function(event){
 //     event.preventDefault();
 //     const name = document.querySelector('#name').value;
@@ -7,14 +7,36 @@ const form = document.querySelector('form');
 //     console.log(name)
 //     console.log(email)
 // });
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    if (name == '') {
-        alert ('Please enter your name');
-    }
-    if (!email.includes('@')){
-        alert('Please enter a valid email');
-    }
-});
+// form.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     const name = document.querySelector('#name');
+//     const email = document.querySelector('#email');
+//     if (name.value == '') {
+//         alert ('Please enter your name');
+//     }
+//     if (!email.value.includes('@')){
+//         alert('Please enter a valid email');
+//     }
+// });
+// const name_ = document.querySelector('#name');
+// const checkbox = document.querySelector('#checkbox');
+
+// form.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     const nameValue = name_.value;
+//     console.log(name_.value)
+// });
+
+// checkbox.addEventListener('change',function(){
+//     const isCheckboxChecked = checkbox.checked;
+//     name_.disabled = !isCheckboxChecked;
+// });
+const select = document.querySelector('#country');
+select.addEventListener('change', () => console.log('Selected country:' + select.value));
+
+const radioButtons = document.querySelectorAll('.gender');
+radioButtons.forEach(function(radio){
+    radio.addEventListener('change', function(){
+        console.log('Selected gender: ' + radio.value);
+    })
+})
